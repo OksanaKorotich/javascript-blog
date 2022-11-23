72f87568b9178a090a2f958535a1994db7957c6f
Oksana Korotich <o.korotich@gmail.com>
2022-11-23 23:00:05 +0100

    Function complite

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 'use strict';
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
 
@@ -11,7 +12,7 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* (done) add class 'active' to the clicked link */
   clickedElement.classList.add('active');
   console.log('clickedElement:', clickedElement);
 
@@ -22,12 +23,17 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* (done) get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute ('href');
+  console.log('articleSelector', clickedElement.getAttribute ('href'));
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* (done) find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle:', targetArticle);
 
   /* add class 'active' to the correct article */
-
+  targetArticle.classList.add('active');
+  
   }
 
   const links = document.querySelectorAll('.titles a');
